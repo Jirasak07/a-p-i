f9e4217c8589a920393e33b0e5a2e57af528b58b
Jirasak07 <bee.110243@gmail.com>
2022-11-18 16:09:19 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var app = express();
-var port = 8080 || process.env.PORT
 
 app.get('/',function(req,res,next){
     res.send("API is Run!!");
 })
 app.listen(2222, function () {
-    console.log("CORS PORT 85");
+    console.log("CORS PORT 2222");
   });
