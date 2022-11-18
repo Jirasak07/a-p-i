cddf0491cfba40368daa6dbd60a6f7f51848b75b
Jirasak07 <bee.110243@gmail.com>
2022-11-18 15:56:42 +0700

    Run

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var port = 8080 || process.env.PORT
 
 app.get('/',function(req,res,next){
-    res.send("API is Run!!");
+    res.send("API is Run!! Run Run Run");
 })
 app.listen(8080||process.env.PORT, function () {
     console.log("app runing port ", PORT );
