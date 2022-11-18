3cd5f6d0b771127a15923c60bbe280aa0c7c9813
Jirasak07 <bee.110243@gmail.com>
2022-11-18 15:55:38 +0700

    change

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var express = require("express");
 var app = express();
-var PORT = 8989
-app.listen(PORT, function () {
-    console.log("app runing port ", PORT );
-  });
+var port = 8080 || process.env.PORT
+
 app.get('/',function(req,res,next){
     res.send("API is Run!!");
 })
+app.listen(8080||process.env.PORT, function () {
+    console.log("app runing port ", PORT );
+  });
 module.exports = app
